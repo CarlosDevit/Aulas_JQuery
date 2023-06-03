2eb4c568aba33e92e64f83d7cecbc846335ea66c
CarlosDevit <carlosdevitsantos@gmail.com>
2023-06-03 16:09:27 -0300

    Exemplo de Eventos

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,30 @@
-$(function(){
-    
-    setTimeout(function(){
-        $('#teste').css('background-color', 'blue')
-        $('#teste p').css('color', 'white').css('font-size', '20px')
-    }, 2000)
-    
+$(function () {
+
+    function validarCliqueHover() {
+        $('.txt1').click(function () {
+            $('.txt2').css('background', 'red').css('color', 'white');
+        })
+
+        $('.txt1').hover(function () {
+            $('.txt2').css('background', 'blue').css('color', 'white');
+        }, function () {
+            $('.txt2').css('background', 'rgb(100, 100, 100)').css('color', 'black');
+        })
+
+        $('textarea').focus(function () {
+            console.log('Estou dentro do textarea!');
+        }).blur(function () {
+            console.log('Estou fora do textarea!');
+        })
+    }
+
+    function validarFormulario() {
+        $('select').change(function () {
+            console.log("Meu select foi alterado!");
+        })
+    }
+
+    validarFormulario();
+    validarCliqueHover();
+
 });

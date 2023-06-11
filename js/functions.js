ab5c1ca9ea42ec810233ac2d60b7e018fafa48b6
CarlosDevit <carlosdevitsantos@gmail.com>
2023-06-11 11:45:01 -0300

    Manipulando textos dinâmicamente

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,31 @@
 $(function () {
-    
-        var box = $('div.box');
 
-        console.log('width sem contar padding: ' + box.width());
-        console.log('height sem contar padding: '+box.height());
-        console.log('width contando padding: '+box.innerWidth());
-        console.log('height contando padding: ' + box.innerHeight());
-        console.log('Outer width : '+box.outerWidth(true));
-        console.log('Outer height : ' + box.outerHeight(true));
+       /* var box = $('.box');
+
+        box.html('<div class="teste"><p>Olá Mundo!</p> ');
+
+        box.html(box.html() +'<div class="teste"><h1 class="texto1">Meu texto em Javascript</h1></div>');
+
+
+      $(b2).text('<div></div>');
+  
+        $(ip).val('Olá Mundo!')*/
+
+        $(ipb).click(function(){
+                var str  = $(ipt).val();
+                //Split separa nossa string com base no delimitador.
+                var var2 = str.split(',');
+                console.log(var2);
+                //substr recorta nossa string
+                console.log(str.substr(0,4));
+
+                var splitstr = str.split('@');
+
+                if(splitstr[1] == 'hotmail.com'){
+                        $(ipt).css('opacity', '0');
+                }else{
+                        console.log('A condicao não bateu')
+                }
+        });
 
 });

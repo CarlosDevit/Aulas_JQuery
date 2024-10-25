473db69dfd6f6c0ce092d1188aa90e10c3bee63c
CarlosDevit <carlosdevitsantos@gmail.com>
2024-10-25 19:48:31 -0300

    Aula Ajax

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,11 @@
 $(function () {
 
-    var timer;
-/*
-    var timeOut = function(){
-        $('.box2').animate({
-            'width': '40%',
-            'height': '500px'
-        }, 2000);
-    };
+     $.ajax({
+          'url': 'conteudo.html',
+          //'method':'post', 
+          //data:{'nome':'carlos','idade':'24'}
+     }).done(function(data){
+          $('#container').append(data);
+     });
 
-    $('body').click(function(){
-        alert("Animação com timeout foi cancelada!");
-        clearTimeout(timer);
-    })
-
-    $('.box1').animate({
-        'width': '40%',
-        'height': '500px'
-    }, 2000, function () {
-        timer = setTimeout(timeOut,3000);
-    });
-*/
-
-    $('body').click(function(){
-        console.log("Intervalo cancelado");
-        clearInterval(timer);
-    });
-
-    timer = setInterval(function(){
-        alert('olá mundo');
-    }, 3000);
 });
